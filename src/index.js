640447818eb4bf162f86d0c138b6e4e343f16830
Beroozam <bmb.mm7@gmail.com>
2021-02-14 23:26:30 +0330

    lecture9-13

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+axios.interceptors.request.use(request => {
+    console.log(request)
+    // Edite request 
+    return request
+},error => {
+    console.log(error)
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+    console.log(response)
+    // Edite response 
+    return response
+},error => {
+    console.log(error)
+    return Promise.reject(error)
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
